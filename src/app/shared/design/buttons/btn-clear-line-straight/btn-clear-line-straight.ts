b9b9c3cff6dda5239ffdc98ee966c5757e821ac4
Miles <miles.pickull@web.de>
2025-07-16 20:53:03 +0200

    Improved Hover effect line when Language is toggled

--- src/app/shared/design/buttons/btn-clear-line-straight/btn-clear-line-straight.ts
+++ src/app/shared/design/buttons/btn-clear-line-straight/btn-clear-line-straight.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, HostBinding } from '@angular/core';
+import { App } from '../../../../app';
 
 @Component({
   selector: 'app-btn-clear-line-straight',
@@ -9,4 +10,24 @@ import { Component, Input } from '@angular/core';
 export class BtnClearLineStraight {
   @Input() text: string = '';
   @Input() link: string = '';
+
+  @ViewChild('textP') textP!: ElementRef;
+
+  @HostBinding('style.--img-wrap-width') imgWrapWidth = '0px';
+  @HostBinding('style.--img-wrap-offset') imgWrapOffset = '0px';
+
+  width = 0;
+  offset = 0
+
+  updateWidth() {
+    if(App.lang === 'langDE'){
+      this.width = 220;
+      this.offset = -10;
+    } else {
+      this.width = 100;
+      this.offset = -30;
+    }
+    this.imgWrapWidth = `${this.width}px`;
+    this.imgWrapOffset = `${this.offset}px`;
+  }
 }
