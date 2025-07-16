6d2457fd902902c6c61345ab6b11951610b6883d
miles <miles.pickull@web.de>
2025-07-17 00:05:14 +0200

    Improved Header Language Switch and Animations

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -5,32 +5,33 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BtnClearCircle } from '../design/buttons/btn-clear-circle/btn-clear-circle';
 import { BtnClearLineStraight } from '../design/buttons/btn-clear-line-straight/btn-clear-line-straight';
 import { BtnClearLineWave } from '../design/buttons/btn-clear-line-wave/btn-clear-line-wave';
+import { BtnClearLineZ } from '../design/buttons/btn-clear-line-z/btn-clear-line-z';
 import * as langDE from './de.json';
 import * as langEN from './en.json';
 import { LangService } from '../../lang-service';
-import { BtnClearLineZ } from '../design/buttons/btn-clear-line-z/btn-clear-line-z';
-
 
 
 @Component({
   selector: 'app-header',
-  imports: [FormsModule, 
-            MatSlideToggleModule, 
-            BtnClearCircle, 
-            BtnClearLineStraight, 
-            BtnClearLineWave,
-            BtnClearLineZ
-          ],
+  imports: [FormsModule,
+    MatSlideToggleModule,
+    BtnClearCircle,
+    BtnClearLineStraight,
+    BtnClearLineWave,
+    BtnClearLineZ,
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 
 export class Header {
   isChecked = false;
-  primary = '#ff7300';
   langDE = langDE;
   langEN = langEN;
   lang = langEN;
+
+  langENclasses = 'active';
+  langDEclasses = '';
 
   constructor(private langService: LangService) { }
 
@@ -40,11 +41,34 @@ export class Header {
     this.langToggle();
   }
 
-  langToggle() { 
+  setLangDE() {
+    App.lang = 'langDE';
+    this.lang = langDE;
+    this.langService.ClickEvent('')
+    this.langENclasses = '';
+    this.langDEclasses = 'active';
+    
+  }
+
+  setLangEN() {
+    App.lang = 'langEN';
+    this.lang = langEN;
+    this.langService.ClickEvent('');
+    this.langENclasses = 'active';
+    this.langDEclasses = '';
+  }
+
+  langToggle() {
     if (App.lang === 'langDE') {
       this.lang = langDE;
+      this.langENclasses = '';
+      this.langDEclasses = 'active';
     } else {
       this.lang = langEN;
-    } 
+      this.langENclasses = 'active';
+      this.langDEclasses = '';
+    }
   }
+
+
 }
