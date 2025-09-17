86309559980223f0eef3225a7423aeddc8d51b3e
miles <miles.pickull@web.de>
2025-09-18 00:17:38 +0200

    feat(legal): added content, title with animation, correct background, language service

--- src/app/subpages/legal-notice/legal-notice.ts
+++ src/app/subpages/legal-notice/legal-notice.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-
+import { App } from '../../app';
+import { TitleLineBlue } from '../../shared/design/titles/title-line-blue/title-line-blue';
+import { LangService } from '../../lang-service';
+import * as langDE from './de.json';
+import * as langEN from './en.json';
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [TitleLineBlue],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
 export class LegalNotice {
+ ngOnInit() {
+    this.langToggle();
+  }
 
+  langDE = langDE;
+  langEN = langEN;
+  lang = langEN;
+  isVisible = false;
+
+  constructor(private langService: LangService) { }
+
+  langToggle() {
+    this.langService.aclickEvent.subscribe((message) => {
+      if (App.lang === 'langDE') {
+        this.lang = langDE;
+      } else {
+        this.lang = langEN;
+      }
+    })
+  }
 }
