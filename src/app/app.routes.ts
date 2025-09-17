328e6f0e9f7854aca9a7095d45bc11e1e17aa86d
miles <miles.pickull@web.de>
2025-09-17 22:29:53 +0200

    feat(legal): added redimental HTML and CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainPage } from './main-page/main-page';
+import { LegalNotice } from './subpages/legal-notice/legal-notice';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', 
+        component: MainPage
+    },
+    {
+        path: 'legal', 
+        component: LegalNotice
+    }
+];
