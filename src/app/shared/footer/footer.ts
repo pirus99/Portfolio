3218c7311d72965914b2f77c78be460d69970fbf
miles <miles.pickull@web.de>
2025-09-18 02:24:48 +0200

    feat(footer): added text Color Input

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,27 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { App } from '../../app';
 import { LangService } from '../../lang-service';
 import * as langDE from './de.json';
 import * as langEN from './en.json';
 import { BtnClearImageRoll } from '../../shared/design/buttons/btn-clear-image-roll/btn-clear-image-roll';
 import { BtnClearLineZ } from '../design/buttons/btn-clear-line-z/btn-clear-line-z';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [BtnClearImageRoll, BtnClearLineZ],
+  imports: [BtnClearImageRoll, BtnClearLineZ, CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
-  ngOnInit() {
-    this.langToggle();
-  }
+  @Input() textColor: string = '';
 
   langDE = langDE;
   langEN = langEN;
   lang = langEN;
 
   constructor(private langService: LangService) { }
+
+  ngOnInit() {
+    this.langToggle();
+  }
 
   langToggle() {
     this.langService.aclickEvent.subscribe((message) => {
