76788ff8065b2b759abc1eb8d0358dab014a1e5e
miles <miles.pickull@web.de>
2025-09-22 01:12:13 +0200

    feat(project-page): added component

---
+++ src/app/subpages/project-page/project-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-page',
+  imports: [],
+  templateUrl: './project-page.html',
+  styleUrl: './project-page.scss'
+})
+export class ProjectPage {
+
+}
