166a41671906384edcd1a8744d983d39be457ebc
miles <miles.pickull@web.de>
2025-09-22 03:33:15 +0200

    feat(project-page): added component to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { MainPage } from './main-page/main-page';
 import { LegalNotice } from './subpages/legal-notice/legal-notice';
 import { PrivacyPolicy } from './subpages/privacy-policy/privacy-policy';
+import { ProjectPage } from './subpages/project-page/project-page';
 
 export const routes: Routes = [
     {
         path: '', 
         component: MainPage
+    },
+    {
+        path: 'projects',
+        component: ProjectPage
     },
     {
         path: 'legal', 
