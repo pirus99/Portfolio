d8575194fb59fd319f3e579ab693781d55ddb9c0
miles <miles.pickull@web.de>
2025-09-23 00:46:51 +0200

    feat(projects): replaced href trough router Link in projects component on main page

--- src/app/main-page/projects/normal-project/normal-project.ts
+++ src/app/main-page/projects/normal-project/normal-project.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { buttonHoverAnimation } from '../../animation-module';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-normal-project',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './normal-project.html',
   styleUrl: './normal-project.scss',
   animations: [buttonHoverAnimation]
