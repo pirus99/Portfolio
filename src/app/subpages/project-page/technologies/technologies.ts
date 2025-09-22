804ca6859eead5fec06855827a749715ee279e7c
miles <miles.pickull@web.de>
2025-09-23 00:48:03 +0200

    feat(technologies-icon): added HTML CSS and Script for small technology Icon

--- src/app/subpages/project-page/technologies/technologies.ts
+++ src/app/subpages/project-page/technologies/technologies.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-technologies',
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './technologies.scss'
 })
 export class Technologies {
+  @Input() imgSrc: string = '';
+  @Input() imgAlt: string = '';
+  @Input() name: string = '';
 
+  ngOnInit() {
+    if (!this.imgSrc) {
+      if (this.name === "HTML") {
+        this.imgSrc = 'img/skills/icons/HTML.svg';
+        this.imgAlt = 'HTML logo';
+      } else if (this.name === "CSS") {
+        this.imgSrc = 'img/skills/icons/CSS.svg';
+        this.imgAlt = 'CSS logo';
+      } else if (this.name === "JavaScript") {
+        this.imgSrc = 'img/skills/icons/Js.svg';
+        this.imgAlt = 'JavaScript logo';
+      } else if (this.name === "TypeScript") {
+        this.imgSrc = 'img/skills/icons/Ts.svg';
+        this.imgAlt = 'TypeScript logo';
+      } else if (this.name === "Angular") {
+        this.imgSrc = 'img/skills/icons/Angular.svg';
+        this.imgAlt = 'Angular logo';
+      } else if (this.name === "Firebase") {
+        this.imgSrc = 'img/skills/icons/Firebase.svg';
+        this.imgAlt = 'Firebase logo';
+      }
+    }
+  }
 }
