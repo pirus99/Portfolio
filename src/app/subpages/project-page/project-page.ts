1b80ba704922083f2498553dcd02c4d6a75a7488
miles <miles.pickull@web.de>
2025-09-22 22:50:33 +0200

    feat(project-page): added minimal CSS and functionality

--- src/app/subpages/project-page/project-page.ts
+++ src/app/subpages/project-page/project-page.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { buttonHoverAnimation } from '../../main-page/animation-module';
+import { TitleLineBlue } from "../../shared/design/titles/title-line-blue/title-line-blue";
 
 @Component({
   selector: 'app-project-page',
-  imports: [],
+  imports: [ TitleLineBlue],
   templateUrl: './project-page.html',
-  styleUrl: './project-page.scss'
+  styleUrl: './project-page.scss',
+  animations: [buttonHoverAnimation]
 })
 export class ProjectPage {
-
+  standardHoverAnimation1 = false;
+  standardHoverAnimation2 = false;
 }
