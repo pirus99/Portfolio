1750abadcf131ebdeff34f37fbda581e4147e323
miles <miles.pickull@web.de>
2025-09-23 02:41:59 +0200

    feat(project-page): added tech icons and some buttons

--- src/app/subpages/project-page/project-page.ts
+++ src/app/subpages/project-page/project-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { buttonHoverAnimation } from '../../main-page/animation-module';
 import { TitleLineBlue } from "../../shared/design/titles/title-line-blue/title-line-blue";
+import { Technologies } from "./technologies/technologies";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-project-page',
-  imports: [ TitleLineBlue],
+  imports: [TitleLineBlue, Technologies, RouterLink],
   templateUrl: './project-page.html',
   styleUrl: './project-page.scss',
   animations: [buttonHoverAnimation]
@@ -12,4 +14,14 @@ import { TitleLineBlue } from "../../shared/design/titles/title-line-blue/title-
 export class ProjectPage {
   standardHoverAnimation1 = false;
   standardHoverAnimation2 = false;
+
+  technologies: { name:string }[] =
+   [
+     { name: "HTML" },
+     { name: "CSS" },
+     { name: "JavaScript" },
+     { name: "TypeScript" },
+     { name: "Angular" },
+     { name: "Firebase" }
+   ];
 }
