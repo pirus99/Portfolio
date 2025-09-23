3446d02a7f427d54dbdf7d75891dc6fa18f0fd0f
miles <miles.pickull@web.de>
2025-09-23 02:08:50 +0200

    feat(header): added half black menu for projects page

--- src/app/shared/design/buttons/btn-clear-line-straight/btn-clear-line-straight.ts
+++ src/app/shared/design/buttons/btn-clear-line-straight/btn-clear-line-straight.ts
@@ -1,15 +1,17 @@
 import { Component, Input, HostBinding } from '@angular/core';
 import { App } from '../../../../app';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-btn-clear-line-straight',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './btn-clear-line-straight.html',
   styleUrl: './btn-clear-line-straight.scss'
 })
 export class BtnClearLineStraight {
   @Input() text: string = '';
   @Input() link: string = '';
+  @Input() dark: boolean = false;
 
   @HostBinding('style.--img-wrap-width') imgWrapWidth = '0px';
   @HostBinding('style.--img-wrap-offset') imgWrapOffset = '0px';
