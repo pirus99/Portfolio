3c72c58d54c9541562d8d5a148768ae9c0e0bb10
miles <miles.pickull@web.de>
2025-09-23 04:25:12 +0200

    feat(project-page): added language Service and more realistic dynamic Data

--- src/app/subpages/project-page/project-page.ts
+++ src/app/subpages/project-page/project-page.ts
@@ -3,6 +3,10 @@ import { buttonHoverAnimation } from '../../main-page/animation-module';
 import { TitleLineBlue } from "../../shared/design/titles/title-line-blue/title-line-blue";
 import { Technologies } from "./technologies/technologies";
 import { RouterLink } from '@angular/router';
+import { LangService } from '../../lang-service';
+import { App } from '../../app';
+import * as langDE from './de.json';
+import * as langEN from './en.json';
 
 @Component({
   selector: 'app-project-page',
@@ -12,8 +16,28 @@ import { RouterLink } from '@angular/router';
   animations: [buttonHoverAnimation]
 })
 export class ProjectPage {
+  projectIndex: number = 2;
   standardHoverAnimation1 = false;
   standardHoverAnimation2 = false;
+  langDE = langDE;
+  langEN = langEN;
+  lang = langEN;
+
+  ngOnInit() {
+    this.langToggle();
+  }
+
+  constructor(private langService: LangService) { }
+
+  langToggle() {
+    this.langService.aclickEvent.subscribe((message) => {
+      if (App.lang === 'langDE') {
+        this.lang = langDE;
+      } else {
+        this.lang = langEN;
+      }
+    })
+  }
 
   technologies: { name:string }[] =
    [
