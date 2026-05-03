a3ed748edc8697e427558caeae453586ff0724cf
miles <miles.pickull@web.de>
2026-05-03 23:46:36 +0200

    feat(title-circle-orange): enhance language support and responsive design adjustments

--- src/app/shared/design/titles/title-cirlce-orange/title-cirlce-orange.ts
+++ src/app/shared/design/titles/title-cirlce-orange/title-cirlce-orange.ts
@@ -13,6 +13,7 @@ export class TitleCirlceOrange {
   @Input() text: string = '';
   @Input() link: string = '';
   @Input() isVisible: boolean = false;
+  @Input() lang = localStorage.getItem('lang') === 'langDE' ? 'langDE' : 'langEN';
 
   @HostBinding('style.--img-wrap-width') imgWrapWidth = '0px';
   @HostBinding('style.--img-wrap-offset') imgWrapOffset = '0px';
@@ -33,19 +34,34 @@ export class TitleCirlceOrange {
     this.langService.aclickEvent.subscribe((message) => {
       setTimeout(() => {
         this.updateWidth();
+        this.ComponentUpdate();
       }, 10);
     })
   }
 
   updateWidth() {
     if (App.lang === 'langDE') {
-      this.width = 600;
-      this.offset = 24;
+      if (window.innerWidth <= 350) {
+        this.offset = -12;
+        this.width = 240;
+      } else {
+        this.offset = 24;
+        this.width = 600;
+      }
     } else {
       this.width = 320;
       this.offset = -12;
     }
     this.imgWrapWidth = `${this.width}px`;
     this.imgWrapOffset = `${this.offset}px`;
+  }
+
+  ComponentUpdate() {
+    const title = document.getElementById('orangeTitle');
+    if (App.lang === 'langDE') {
+      title?.classList.add('lang-de');
+    } else {
+      title?.classList.remove('lang-de');
+    }
   }
 }
